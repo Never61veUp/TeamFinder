b0a55e21c0c300bbed15435ffdcbbe1b867e9f25
Aleksey <134203994+Never61veUp@users.noreply.github.com>
2026-04-21 16:13:29 +0500

    feat: add core models and services for skill management

---
+++ backend/TeamFinder.Core/Model/Profile.cs
@@ -0,0 +1,38 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace TeamFinder.Core.Model;
+
+public class Profile : Entity<Guid>
+{
+    private readonly HashSet<Guid> _skillIds = [];
+
+    public IReadOnlyCollection<Guid> SkillIds => _skillIds;
+
+    private Profile() { }
+
+    public void AddSkill(Guid skillId)
+    {
+        _skillIds.Add(skillId);
+    }
+}
+
+public class Skill : Entity<Guid>
+{
+    public string Name { get; }
+
+    public Skill(string name)
+    {
+        Name = name;
+    }
+
+}
+
+public class SkillRelation
+{
+    public Guid Id { get; private set; }
+
+    public Guid ParentSkillId { get; private set; }
+    public Guid ChildSkillId { get; private set; }
+
+    public double Weight { get; private set; }
+}
---
+++ backend/TeamFinder.Postgresql/ISkillRelationRepository.cs
@@ -0,0 +1,8 @@
+﻿using TeamFinder.Core.Model;
+
+namespace TeamFinder.Postgresql;
+
+public interface ISkillRelationRepository
+{
+    Task<List<SkillRelation>> GetByParentId(Guid parentId);
+}
