8b81eb5906293fe21ff80febaf2e7970a6a66e9c
Aleksey <134203994+Never61veUp@users.noreply.github.com>
2026-04-22 13:21:41 +0500

    feat: add Telegram ID column to user profiles

---
+++ backend/TeamFinder.Postgresql/Migrations/20260422080838_pairTgWithProfile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamFinder.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class pairTgWithProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TgId",
+                table: "profiles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TgId",
+                table: "profiles");
+        }
+    }
+}
