f75990fd80e897de8ca2e2767b9e5dfb1c9a5068
Aleksey <134203994+Never61veUp@users.noreply.github.com>
2026-04-29 14:42:50 +0500

    tags migration

---
+++ backend/TeamFinder.Postgresql/Migrations/20260429094203_tagsDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamFinder.Postgresql.Migrations
+{
+    /// <inheritdoc />
+    public partial class tagsDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "EventTags",
+                table: "teams",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventTags",
+                table: "teams");
+        }
+    }
+}
